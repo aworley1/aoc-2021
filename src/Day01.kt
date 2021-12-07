d572e9b10ed4c28ab768aa4d026c0a4b6fca5c15
Andrew Worley <andrew.worley@johnlewis.co.uk>
2021-12-07 11:02:52 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,29 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.windowed(2)
+            .map {
+                if (it[1].toInt() > it[0].toInt()) 1 else 0
+            }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .windowed(3)
+            .windowed(2)
+            .map {
+                if (it[1].sum() > it[0].sum()) 1 else 0
+            }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val part1TestOutput = part1(testInput)
+    println(part1TestOutput)
+    check(part1TestOutput == 7)
 
     val input = readInput("Day01")
     println(part1(input))
+
+    check(part2(testInput) == 5)
     println(part2(input))
 }
