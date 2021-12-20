22587074cde91b2d4e40d051be3d86b91c449fc0
Andrew Worley <andrew.worley@johnlewis.co.uk>
2021-12-20 23:09:52 +0000

    Day 4 part 2

--- src/main/kotlin/day04/Day04.kt
+++ src/main/kotlin/day04/Day04.kt
@@ -4,22 +4,30 @@ import readInput
 
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val game = parseGame(input)
+
+        val (card, number) = game.firstWinningCard()
+
+        return card.squares.values.filterNot { it.marked }.sumOf { it.number } * number
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val game = parseGame(input)
+
+        val (card, number) = game.lastWinningCard()
+
+        return card.squares.values.filterNot { it.marked }.sumOf { it.number } * number
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("day04/Day04_test")
+    val testInput = readInput("main/kotlin/day04/Day04_test")
     val part1TestOutput = part1(testInput)
     println(part1TestOutput)
     check(part1TestOutput == 4512)
 
-    val input = readInput("day04/Day04")
+    val input = readInput("main/kotlin/day04/Day04")
     println(part1(input))
 
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 1924)
     println(part2(input))
 }
--- src/main/kotlin/day04/Game.kt
+++ src/main/kotlin/day04/Game.kt
@@ -3,16 +3,67 @@ package day04
 data class Game(
     val numbers: List<Int>,
     val cards: List<Card>
-)
+) {
 
-data class Card(val squares: List<Square>)
+    fun firstWinningCard(): Pair<Card, Int> {
+        val cardsOverTime = numbers.fold(listOf(cards)) { acc, num ->
+            acc + listOf(acc.last().map { it.mark(num) })
+        }
+
+        val firstWinningTimeIndex = cardsOverTime.mapIndexedNotNull { index, cards ->
+            if (cards.any { it.hasWon() }) index else null
+        }.first()
+
+        val firstWinningCardIndex =
+            cardsOverTime[firstWinningTimeIndex].mapIndexedNotNull { index, card -> if (card.hasWon()) index else null }
+                .single()
+
+        return Pair(cardsOverTime[firstWinningTimeIndex][firstWinningCardIndex], numbers[firstWinningTimeIndex - 1])
+    }
+
+    fun lastWinningCard(): Pair<Card, Int> {
+        val cardsOverTime = numbers.fold(listOf(cards)) { acc, num ->
+            acc + listOf(acc.last().map { it.mark(num) })
+        }
+
+        val lastWinningTimeIndex = cardsOverTime.mapIndexedNotNull { index, cards ->
+            if (cards.all { it.hasWon() }) index else null
+        }.first()
+
+        val lastWinningCardIndex: Int = cardsOverTime[lastWinningTimeIndex - 1].mapIndexedNotNull { index, card ->
+            if (!card.hasWon()) index else null
+        }.single()
+
+        return Pair(cardsOverTime[lastWinningTimeIndex][lastWinningCardIndex], numbers[lastWinningTimeIndex - 1])
+    }
+
+}
+
+data class Card(val squares: Map<Coord, Square>) {
+    constructor(list: List<Square>) : this(list.associateBy { it.coord })
+
+    private val rows = squares.keys.map { it.row }.distinct()
+    private val cols = squares.keys.map { it.column }.distinct()
+    private val rowCoords = rows.map { row -> cols.map { Coord(row = row, column = it) } }
+    private val colCoords = cols.map { col -> rows.map { Coord(row = it, column = col) } }
+
+    fun mark(number: Int) = Card(squares.mapValues { it.value.mark(number) })
+
+    fun hasWon(): Boolean {
+        return colCoords.any { column -> column.all { squares[it]!!.marked } } ||
+                rowCoords.any { row -> row.all { squares[it]!!.marked } }
+    }
+}
 
 data class Square(
-    val row: Int,
-    val column: Int,
+    val coord: Coord,
     val number: Int,
     val marked: Boolean = false
-)
+) {
+    fun mark(number: Int) = if (number == this.number) this.copy(marked = true) else this
+}
+
+data class Coord(val row: Int, val column: Int)
 
 fun parseGame(input: List<String>): Game {
     val numbers = input.first().split(",").map { it.toInt() }
@@ -26,8 +77,10 @@ fun parseGame(input: List<String>): Game {
                     row.trim().split("\\s+".toRegex())
                         .mapIndexed { colNum, num ->
                             Square(
-                                row = rowNum,
-                                column = colNum,
+                                coord = Coord(
+                                    row = rowNum,
+                                    column = colNum
+                                ),
                                 number = num.toInt()
                             )
                         }
