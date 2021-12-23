3083d15b4b27a11f15fc342f87a1575b29b514a9
Andrew Worley <andrew.worley@johnlewis.co.uk>
2021-12-23 16:17:07 +0000

    Change template so IntelliJ doesn't think it's in error

--- create-day.sh
+++ create-day.sh
@@ -5,5 +5,5 @@ export DAY_NAME="Day$day_num"
 export DAY_DIR="day$day_num"
 
 mkdir src/main/kotlin/$DAY_DIR
-envsubst < template/Day.kt > src/main/kotlin/$DAY_DIR/${DAY_NAME}.kt
+envsubst < template/Daykt > src/main/kotlin/$DAY_DIR/${DAY_NAME}.kt
 touch src/main/kotlin/$DAY_DIR/${DAY_NAME}.txt src/main/kotlin/$DAY_DIR/${DAY_NAME}_test.txt
